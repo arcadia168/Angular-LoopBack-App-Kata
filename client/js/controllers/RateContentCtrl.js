7bd7a5f5637d2e7dabe86679146b22333f1fb5b3
Erin Eland <erin@erineland.com>
2016-01-10 21:08:33 +0000

    Implemented a directive to display star ratings using font-awesome, and persist any changes to star ratings to the backend server

--- client/js/controllers/RateContentCtrl.js
+++ client/js/controllers/RateContentCtrl.js
@@ -1,11 +1,15 @@
 angular.module('app').controller('RateContentController', ['$scope', 'Content', function ($scope,
                                                                                          Content) {
-  //controller code here
-
+  //first retrieve all content items from backend and attach to scope.
   Content.find()
     .$promise
     .then(function(content) {
       $scope.content = content;
     });
+
+  $scope.updateRating = function(contentItem) {
+    //use service to update the rating of the show on the backend server
+    contentItem.$save();  //returns a promise
+  };
 
 }]);
