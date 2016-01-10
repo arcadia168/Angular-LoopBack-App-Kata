002ecd69bb9d3115d45a3802f8d3d6c4f35ff297
Erin Eland <erin@erineland.com>
2016-01-10 21:15:22 +0000

    Tidied up custom directive for star rating, moved template out into a partial view

--- client/js/directives/StarRating.js
+++ client/js/directives/StarRating.js
@@ -1,47 +1,41 @@
-angular.module('app').directive('starRating', ['$compile', function ($compile) {
+angular.module('app').directive('starRating', [function () {
+
+  var link = function (scope) {
+    if (scope.max == undefined) {
+      scope.max = 5;
+    }
+    function updateStars() {
+      scope.stars = [];
+      for (var i = 0; i < scope.max; i++) {
+        scope.stars.push({
+          filled: i < scope.ratingValue
+        });
+      }
+    }
+    scope.toggle = function (index) {
+      if (scope.readonly == undefined || scope.readonly === false) {
+        scope.ratingValue = index + 1;
+        scope.onRatingSelect({
+          rating: index + 1
+        });
+      }
+    };
+    scope.$watch('ratingValue', function (oldValue, newValue) {
+      if (newValue) {
+        updateStars();
+      }
+    });
+  };
 
   return {
-    restrict: 'EA',
-    template:
-    '<ul class="star-rating" ng-class="{readonly: readonly}">' +
-    '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)">' +
-    '    <i class="fa fa-star"></i>' + // or &#9733
-    '  </li>' +
-    '</ul>',
+    restrict: 'E',
+    templateUrl: 'views/partials/star-rating.html',
     scope: {
-      ratingValue: '=ngModel',
+      ratingValue: '=',
       max: '=?', // optional (default is 5)
       onRatingSelect: '&?',
       readonly: '=?'
     },
-    link: function(scope, element, attributes) {
-      //debugger;
-      if (scope.max == undefined) {
-        scope.max = 5;
-      }
-      function updateStars() {
-        //debugger;
-        scope.stars = [];
-        for (var i = 0; i < scope.max; i++) {
-          scope.stars.push({
-            filled: i < scope.ratingValue
-          });
-        }
-      };
-      scope.toggle = function(index) {
-        if (scope.readonly == undefined || scope.readonly === false){
-          scope.ratingValue = index + 1;
-          scope.onRatingSelect({
-            rating: index + 1
-          });
-        }
-      };
-      scope.$watch('ratingValue', function(oldValue, newValue) {
-        //debugger;
-        if (newValue) {
-          updateStars();
-        }
-      });
-    }
+    link: link
   };
 }]);
