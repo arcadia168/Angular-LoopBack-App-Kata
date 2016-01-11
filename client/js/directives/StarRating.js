8ca339b158666cb1dc38206d9b1694f37273822d
Erin Eland <erin@erineland.com>
2016-01-11 01:10:12 +0000

    Final tweaks, moving all client dependencies to bower

--- client/js/directives/StarRating.js
+++ client/js/directives/StarRating.js
@@ -15,9 +15,9 @@ angular.module('app').directive('starRating', [function () {
     scope.toggle = function (index) {
       if (scope.readonly == undefined || scope.readonly === false) {
         scope.ratingValue = index + 1;
-        scope.onRatingSelect({
-          rating: index + 1
-        });
+        //scope.onRatingSelect({
+        //  rating: index + 1
+        //});
       }
     };
     scope.$watch('ratingValue', function (oldValue, newValue) {
@@ -33,7 +33,6 @@ angular.module('app').directive('starRating', [function () {
     scope: {
       ratingValue: '=',
       max: '=?', // optional (default is 5)
-      onRatingSelect: '&?',
       readonly: '=?'
     },
     link: link
